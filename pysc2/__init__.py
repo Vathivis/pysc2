c7a56621711a4115cd71cf3b77bd2919e541b102
Vathivis <vojtahumpl@seznam.cz>
2025-11-09 22:32:36 +0100

    Enhance SC2 run configuration and update supported Python versions
    - Introduce optional window location and size parameters for SC2 client.
    - Add functions to set additional SC2 binary arguments.
    - Update setup.py to support Python 3.12 and 3.13.

--- pysc2/__init__.py
+++ pysc2/__init__.py
@@ -14,6 +14,49 @@
 """PySC2 module: https://github.com/deepmind/pysc2 ."""
 
 import os
+import random
+from typing import Callable, MutableSequence, Optional, TypeVar
+
+from absl import flags
+
+_T = TypeVar("_T")
+
+
+def _legacy_shuffle(seq: MutableSequence[_T], rand_fn: Callable[[], float]) -> None:
+  for i in range(len(seq) - 1, 0, -1):
+    j = int(rand_fn() * (i + 1))
+    seq[i], seq[j] = seq[j], seq[i]
+
+
+def _patch_random_shuffle() -> None:
+  """Re-introduce the optional `random` argument removed in Python 3.11."""
+  try:
+    random.shuffle([0, 1], lambda: 0.5)
+  except TypeError:
+    original_shuffle = random.shuffle
+
+    def shuffle(seq: MutableSequence[_T],
+                rand_fn: Optional[Callable[[], float]] = None) -> None:
+      if rand_fn is None:
+        original_shuffle(seq)
+      else:
+        _legacy_shuffle(seq, rand_fn)
+
+    random.shuffle = shuffle  # type: ignore[assignment]
+
+
+def _ensure_absl_flags_parsed() -> None:
+  """Call absl's flags parser if the caller has not done so yet."""
+  flags_obj = flags.FLAGS
+  is_parsed = getattr(flags_obj, "is_parsed", None)
+  if callable(is_parsed) and is_parsed():
+    return
+  # The module name is irrelevant; we just need any argv-like iterable.
+  flags_obj([__name__])
+
+
+_patch_random_shuffle()
+_ensure_absl_flags_parsed()
 
 
 def load_tests(loader, standard_tests, unused_pattern):
--- pysc2/run_configs/__init__.py
+++ pysc2/run_configs/__init__.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Configs for various ways to run starcraft."""
 
+from typing import Optional, Sequence, Tuple
+
 from absl import flags
 
 from pysc2.lib import sc_process
@@ -22,6 +24,68 @@ from pysc2.run_configs import lib
 flags.DEFINE_string("sc2_run_config", None,
                     "Which run_config to use to spawn the binary.")
 FLAGS = flags.FLAGS
+
+_SC2_EXTRA_ARGS: Sequence[str] = ()
+_SC2_WINDOW_LOC: Optional[Tuple[int, int]] = None
+_SC2_WINDOW_SIZE: Optional[Tuple[int, int]] = None
+
+
+class _RunConfigProxy(object):
+  """Proxy that injects global SC2 launch options."""
+
+  def __init__(self, base_cfg: lib.RunConfig,
+               extra_args: Sequence[str],
+               window_loc: Optional[Tuple[int, int]],
+               window_size: Optional[Tuple[int, int]]):
+    self._base = base_cfg
+    self._extra_args = list(extra_args)
+    self._window_loc = window_loc
+    self._window_size = window_size
+
+  def __getattr__(self, item):
+    return getattr(self._base, item)
+
+  def start(self, *args, **kwargs):
+    extra = list(kwargs.pop("extra_args", []))
+    extra.extend(self._extra_args)
+    kwargs["extra_args"] = extra
+    if self._window_loc is not None and "window_loc" not in kwargs:
+      kwargs["window_loc"] = self._window_loc
+    if self._window_size is not None and "window_size" not in kwargs:
+      kwargs["window_size"] = self._window_size
+    return self._base.start(*args, **kwargs)
+
+
+def _apply_run_config_overrides(cfg: lib.RunConfig) -> lib.RunConfig:
+  if (_SC2_EXTRA_ARGS or
+      _SC2_WINDOW_LOC is not None or
+      _SC2_WINDOW_SIZE is not None):
+    return _RunConfigProxy(cfg, _SC2_EXTRA_ARGS, _SC2_WINDOW_LOC, _SC2_WINDOW_SIZE)
+  return cfg
+
+
+def _normalize_window_tuple(
+    value: Optional[Sequence[int]],
+    label: str) -> Optional[Tuple[int, int]]:
+  if value is None:
+    return None
+  if len(value) != 2:
+    raise ValueError("%s must contain exactly two integers" % label)
+  return (int(value[0]), int(value[1]))
+
+
+def set_sc2_extra_args(extra_args: Optional[Sequence[str]]) -> None:
+  """Configure additional StarCraft II binary args (e.g., --norender)."""
+  global _SC2_EXTRA_ARGS
+  _SC2_EXTRA_ARGS = tuple(str(arg) for arg in (extra_args or ()))
+
+
+def set_sc2_window_config(window_loc: Optional[Sequence[int]],
+                          window_size: Optional[Sequence[int]]) -> None:
+  """Configure SC2 window placement/size for the next launched client."""
+  global _SC2_WINDOW_LOC, _SC2_WINDOW_SIZE
+  _SC2_WINDOW_LOC = _normalize_window_tuple(window_loc, "window_loc")
+  _SC2_WINDOW_SIZE = _normalize_window_tuple(window_size, "window_size")
 
 
 def get(version=None):
@@ -33,10 +97,12 @@ def get(version=None):
     raise sc_process.SC2LaunchError("No valid run_configs found.")
 
   if FLAGS.sc2_run_config is None:  # Find the highest priority as default.
-    return max(configs.values(), key=lambda c: c.priority())(version=version)
+    config_cls = max(configs.values(), key=lambda c: c.priority())
+    return _apply_run_config_overrides(config_cls(version=version))
 
   try:
-    return configs[FLAGS.sc2_run_config](version=version)
+    return _apply_run_config_overrides(
+        configs[FLAGS.sc2_run_config](version=version))
   except KeyError:
     raise sc_process.SC2LaunchError(
         "Invalid run_config. Valid configs are: %s" % (
